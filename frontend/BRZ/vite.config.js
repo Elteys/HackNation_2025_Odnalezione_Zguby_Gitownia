3c385b5c0944ca5bdfcd792c50ceb55c4e37a602
jas <126872967+olszlug@users.noreply.github.com>
2025-12-07 10:00:18 +0100

    del kom

--- frontend/BRZ/vite.config.js
+++ frontend/BRZ/vite.config.js
@@ -1,21 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import fs from 'fs' // <--- Potrzebne do odczytu plików certyfikatów
+import fs from 'fs'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <--- Twój plugin Tailwind
+    tailwindcss(),
   ],
   server: {
-    // Konfiguracja HTTPS (zielona kłódka)
     https: {
       key: fs.readFileSync('./localhost-key.pem'),
       cert: fs.readFileSync('./localhost.pem'),
     },
-    // Opcjonalnie: Pozwala na dostęp z innych urządzeń w sieci (np. telefonu)
     host: true, 
   },
 })
